495d79ee6951510e63975da1ddd150875ce941bd
mohi14 <mohiuddinkhan1409@gmail.com>
2024-01-22 12:49:16 +0600

    change cors url

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ app.use(cookieParser());
 // CORS Middleware
 const corsOptions = {
   // origin: "http://localhost:3001",
-  origin: "https://virtual-notes-react-uz5y.vercel.app",
+  origin: [
+    "http://localhost:3000",
+    "https://virtual-notes-react-uz5y.vercel.app",
+  ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
